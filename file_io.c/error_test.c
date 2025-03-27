b900038f8ba7c812093ff33668b86e6ae5e4c4c0
Likhitha A L <likhithaal@Mac.lan>
2025-03-27 21:04:03 +0530

    error types

--- file_io.c/error_test.c
+++ file_io.c/error_test.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <errno.h>
 #include <fcntl.h>
-
+#include <string.h>
+/*
+error type:
+errno (it will give the error number)
+strerror() (it will give the exact meaning of errno)
+perror() (directly it will give what exactly the error is)
+*/
 int main(){
     int fd=0;
     printf("initial errno: %d\n",errno);
@@ -9,6 +15,8 @@ int main(){
     fd = open("haha.txt",O_RDONLY);
     if(fd == -1){
         printf("errno : %d\n",errno);
+        printf("errorno string is %s\n",strerror(errno));
+        perror("error is: ");     
         return -1;
     }
     return 0;
