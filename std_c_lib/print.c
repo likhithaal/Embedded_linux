52541af3723cb1ae654a902d7e7588658bece8e7
Likhitha A L <likhithaal@Mac.lan>
2025-04-02 23:49:26 +0530

    print statements format

--- std_c_lib/print.c
+++ std_c_lib/print.c
@@ -1,11 +1,32 @@
 #include <stdio.h>
+#include <fcntl.h>
 #include <unistd.h>
 
 int main(){
+    FILE *f = NULL;
+    int ret = 0;
+    char buf[128] = {0};
 
-    printf("hello world\n");
+    f = fopen("./test.txt","w+");  //w+ O_RDWR | O_CREATE mode 
+    if(f == NULL){
+        perror("fopen err");
+        return -1;
+    }
 
-    write(1,"hello world",11);  //same as printf fd = 1 for stdout
+    printf("hello world printf\n");
+    write(1,"hello write\n",11);  //same as printf fd = 1 for stdout
+    
+    fprintf(f,"hiii hello fprintf\n");   //to print on file using file *stream
+    fprintf(stdout,"hii hello fprintf stdout\n");  //stdout fd = 1   --- to print on console
 
+    dprintf(1,"hii dprintf\n");  //to print on file using fd
+
+    sprintf(buf,"print buffer\n"); //store in buf
+    printf("%s\n",buf); //printf that buf
+
+    snprintf(buf,128,"print size cntl buffer\n");
+    printf("%s\n",buf);  //print the buf
+
+    fclose(f);
     return 0;
 }
