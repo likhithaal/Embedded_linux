f26fa8da223491bf0f002e40e8d036d412ef5b70
Likhitha A L <likhithaal@Mac.lan>
2025-04-24 20:45:08 +0530

    pid

---
+++ process_concept/pid.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <string.h>
+#include <utime.h>
+#include <time.h>
+#include <sys/time.h>
+#include <signal.h>
+
+int main(int argc,char *argv[]){
+    printf("Pid is %d\n",getpid());
+    return 0;
+}
