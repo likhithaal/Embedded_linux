23b247fd3ee53e56da6ee663a7c60b3b590401b2
Likhitha A L <likhithaal@Vijeths-MacBook-Air.local>
2025-05-12 20:28:31 +0530

    writing on file in fork

--- process_concept/file.c
+++ process_concept/file.c
@@ -11,24 +11,28 @@
 
 int main(int argc,char *argv[]){
 
-    int ret = 0;
-    // printf("pid:%d parent pid:%d\n",getpid(),getppid());
-    // fork returns 2 times one for parent and other for child
-    // child is the copy of parent
-    ret = fork();
-    if(ret == -1){
-        printf("fork err\n");
+    int ret = 0,fd = 0;
+    fd = open("./test.txt",O_WRONLY|O_TRUNC);
+    if(fd == -1){
+        printf("open err\n");
         return -1;
     }
-    else if(ret == 0){
-        //child process
-        printf("child pid:%d parent pid:%d\n",getpid(),getppid());
+    printf("hello\n");
+
+    switch (fork())
+    {
+        case -1:
+            printf("fork err\n");
+            close(fd);
+            return -1;
+        case 0:
+            printf("child process\n");
+            write(fd,"child says hello",17);
+            return 0;
+    default:
+        printf("parent process\n");
+        write(fd,"parent says hello",18);
+        return 0;
     }
-    else{
-        //parent process
-        printf("parent pid:%d parent parent pid:%d child pid:%d\n",getpid(),getppid(),ret);
-    }
-    // printf("fork val: %d\n",ret);
-    // printf("hii\n");
     return 0;
 }
